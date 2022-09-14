8baf3288cff7410afe73cf3a6cc990e0105357f2
hirunaa <tmfrl040622@naver.com>
2022-09-14 13:00:52 +0900

    수정

--- js/text2.js
+++ js/text2.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
     console.log("asdfasdf");
+    $("._2hvTZ, .pexuQ, .zyHYP").on("input", function(e){
+        if($(this).attr("name")==="username")
+        {
+            if($(this).val()!="")
+            {
+                console.log("dd");
+                $("#userName").text("");
+                
+            }
+            else $("#userName").text("전화번호, 사용자 이름 또는 이메일");
+        }
+        else if($(this).attr("name")==="password")
+        {
+            if($(this).val()!="")
+            {
+                console.log("dd");
+                $("#passWord").text("");
+                
+            }
+            else $("#passWord").text("비밀번호");
+        }
+    });
 });
