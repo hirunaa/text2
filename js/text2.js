b7ffd48d76864119500f2e30f298f7f07869b07b
hirunaa <tmfrl040622@naver.com>
2022-09-14 17:01:48 +0900

    수정

--- js/text2.js
+++ js/text2.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
     let usernameId = 0;
     let idPassword = 0;
+    
+    let userList= ["asdfasdf", "qwerasdf"];
+    let passwordList = ["123456", "654321"];
+
     $("._2hvTZ, .pexuQ, .zyHYP").on("input", function(e){
         if($(this).attr("name")==="username")
         { 
             usernameId = $(this).val();
-            console.log(usernameId);
             if($(this).val()!="")
             {
                 $(".f0n8F:eq(0)").addClass("FATdn");
@@ -15,7 +18,6 @@ $(document).ready(function(){
         else if($(this).attr("name")==="password")
         {
             idPassword = $(this).val();
-            console.log(idPassword);
             if($(this).val()!="")
             {
                 $(".f0n8F:eq(1)").addClass("FATdn"); 
@@ -28,6 +30,34 @@ $(document).ready(function(){
             $(".sqdOP, .L3NKy, .y3zKF").eq(0).removeAttr("disabled");
         }
         else $(".sqdOP, .L3NKy, .y3zKF").eq(0).attr("disabled", "");
+    });
+
+    $(".sqdOP, .L3NKy, .y3zKF").eq(0).on("click", function(e){
+        var userListIndex=-1;
+        var passwordListIndex=-2;
+        for(var i=0; i<userList.length; i++)
+        {
+            if(userList[i]==usernameId)
+            {
+                userListIndex=i;
+            }
+            
+            if(passwordList[i]==idPassword)
+            {
+                passwordListIndex=i;
+            }
+            
+        }
+        if(userListIndex==passwordListIndex)
+        {
+            e.preventDefault();
+            console.log("ㅇㅇ");
+        }
+        else
+        {
+            e.preventDefault();
+            console.log("ㄴㄴ");
+        }
     });
 
 
