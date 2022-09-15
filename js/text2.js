2c695d63a9459e1f33104dfe8c404a70177af89a
hirunaa <tmfrl040622@naver.com>
2022-09-15 09:53:00 +0900

    수정

--- js/text2.js
+++ js/text2.js
@@ -4,57 +4,61 @@ $(document).ready(function(){
     
     let userList= ["asdfasdf", "qwerasdf"];
     let passwordList = ["123456", "654321"];
+
+    var inputBox = $("._2hvTZ, .pexuQ, .zyHYP");
+    var loginButton = $(".sqdOP, .L3NKy, .y3zKF");
+
     function inputFocusInit(){
-        $("._9GP1n").removeClass("HlU5H");
-        $("._2hvTZ, .pexuQ, .zyHYP").removeClass("focus-visible");
-        $("._2hvTZ, .pexuQ, .zyHYP").removeAttr("data-focus-visible-added");
+        $(".inputContainer").removeClass("HlU5H");
+        inputBox.removeClass("focus-visible");
+        inputBox.removeAttr("data-focus-visible-added");
     }
-    $("._2hvTZ, .pexuQ, .zyHYP").on("focus", function(){
+    inputBox.on("focus", function(){
         inputFocusInit();
         if($(this).attr("name")==="username")
         {
-            $("._9GP1n:eq(0)").addClass("HlU5H");
+            $(".inputContainer:eq(0)").addClass("HlU5H");
             $(this).addClass("focus-visible");
             $(this).attr("data-focus-visible-added","");
         }
         if($(this).attr("name")==="password")
         {
-            $("._9GP1n:eq(1)").addClass("HlU5H");
+            $(".inputContainer:eq(1)").addClass("HlU5H");
             $(this).addClass("focus-visible");
             $(this).attr("data-focus-visible-added","");
         }
     });
-    $("._2hvTZ, .pexuQ, .zyHYP").on("blur",function(){
+    inputBox.on("blur",function(){
         inputFocusInit();
     });
-    $("._2hvTZ, .pexuQ, .zyHYP").on("input", function(){
+    inputBox.on("input", function(){
         if($(this).attr("name")==="username")
         { 
             usernameId = $(this).val();
             if($(this).val()!="")
             {
-                $(".f0n8F:eq(0)").addClass("FATdn");
+                $(".idPasswordInput:eq(0)").addClass("placeholderScale");
             }
-            else $(".f0n8F:eq(0)").removeClass("FATdn");
+            else $(".idPasswordInput:eq(0)").removeClass("placeholderScale");
         }
         else if($(this).attr("name")==="password")
         {
             idPassword = $(this).val();
             if($(this).val()!="")
             {
-                $(".f0n8F:eq(1)").addClass("FATdn"); 
+                $(".idPasswordInput:eq(1)").addClass("placeholderScale"); 
             }
-            else $(".f0n8F:eq(1)").removeClass("FATdn");
+            else $(".idPasswordInput:eq(1)").removeClass("placeholderScale");
         }
 
         if(usernameId.length>=1 && idPassword.length>=6)
         {
-            $(".sqdOP, .L3NKy, .y3zKF").eq(0).removeAttr("disabled");
+            loginButton.eq(0).removeAttr("disabled");
         }
-        else $(".sqdOP, .L3NKy, .y3zKF").eq(0).attr("disabled", "");
+        else loginButton.eq(0).attr("disabled", "");
     });
 
-    $(".sqdOP, .L3NKy, .y3zKF").eq(0).on("click", function(e){
+    loginButton.eq(0).on("click", function(e){
         var userListIndex=-1;
         var passwordListIndex=-2;
         for(var i=0; i<userList.length; i++)
@@ -73,13 +77,13 @@ $(document).ready(function(){
         if(userListIndex==passwordListIndex)
         {
             //e.preventDefault();
-            console.log("ㅇㅇ");
+            //console.log("ㅇㅇ");
             $(".eiCW-").remove();
         }
         else
         {
             e.preventDefault();
-            console.log("ㄴㄴ");
+            //console.log("ㄴㄴ");
             $("#facebookLogin").eq(0).after(
                 '<div class="eiCW-"><p aria-atomic="true" data-testid="login-error-message" id="slfErrorAlert" role="alert">잘못된 비밀번호입니다. 다시 확인하세요.</p></div>');
         }
